2ea55afe245dee6aa3c6dade612de80c2c157f3c
Christopher Nathanael Tessy <christopher.nathanael1217@gmail.com>
2025-02-27 12:32:55 +0700

    fix: husky

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 
 import Image from "next/image";
 
@@ -24,7 +24,7 @@ export default function ProjectDetails({
 
   if (!project) {
     return (
-      <div className="text-white text-center mt-10">Project not found</div>
+      <div className="mt-10 text-center text-white">Project not found</div>
     );
   }
 
@@ -32,9 +32,9 @@ export default function ProjectDetails({
     <main className="bg-black">
       <Navbar />
 
-      <section className="min-h-screen flex flex-col px-[8%] md:px-[10%] lg:px-[12%] py-32 gap-16 text-white">
+      <section className="flex min-h-screen flex-col gap-16 px-[8%] py-32 text-white md:px-[10%] lg:px-[12%]">
         <button
-          className="inline-flex px-7 h-12 animate-shimmer items-center justify-center rounded-lg border border-slate-400 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] text-sm font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 focus:ring-offset-slate-50 gap-2 w-fit"
+          className="inline-flex h-12 w-fit animate-shimmer items-center justify-center gap-2 rounded-lg border border-slate-400 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-7 text-sm font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 focus:ring-offset-slate-50"
           onClick={() => router.back()}
         >
           <svg
@@ -55,13 +55,13 @@ export default function ProjectDetails({
         </button>
 
         <div
-          className="relative flex items-center justify-center w-full max-w-[800px] aspect-[16/10] overflow-hidden group bg-cover rounded-lg border border-white/[0.2] group-hover:border-white/[0.4] transition duration-700 self-center"
+          className="group relative flex aspect-[16/10] w-full max-w-[800px] items-center justify-center self-center overflow-hidden rounded-lg border border-white/[0.2] bg-cover transition duration-700 group-hover:border-white/[0.4]"
           style={{
             backgroundImage: "url('/projects/bg.png')",
             backgroundColor: "#13162D",
           }}
         >
-          <div className="absolute overflow-hidden w-full h-full rounded-lg rotate-2 top-12 left-6">
+          <div className="absolute left-6 top-12 h-full w-full rotate-2 overflow-hidden rounded-lg">
             <Image
               src={project.img}
               alt="cover"
@@ -72,7 +72,7 @@ export default function ProjectDetails({
         </div>
 
         <div className="flex flex-col gap-4">
-          <p className="uppercase tracking-widest text-blue-100 max-w-80">
+          <p className="max-w-80 uppercase tracking-widest text-blue-100">
             {project.category}
           </p>
           <h1 className="text-5xl font-bold">{project.title}</h1>
@@ -107,9 +107,9 @@ export default function ProjectDetails({
             href={project.screenshotLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex text-sm font-medium w-full"
+            className="flex w-full text-sm font-medium"
           >
-            <button className="inline-flex px-6 h-12 animate-shimmer items-center justify-center rounded-lg border border-slate-400 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 focus:ring-offset-slate-50 w-full">
+            <button className="inline-flex h-12 w-full animate-shimmer items-center justify-center rounded-lg border border-slate-400 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               Screenshots
             </button>
           </Link>
@@ -119,21 +119,21 @@ export default function ProjectDetails({
               href={project.liveLink}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex text-sm font-medium w-full"
+              className="flex w-full text-sm font-medium"
             >
-              <button className="inline-flex px-6 h-12 animate-shimmer items-center justify-center gap-2 rounded-lg border border-emerald-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] text-emerald-100 transition-colors focus:outline-none focus:ring-2 focus:ring-emerald-400 focus:ring-offset-2 focus:ring-offset-emerald-50 w-full">
+              <button className="inline-flex h-12 w-full animate-shimmer items-center justify-center gap-2 rounded-lg border border-emerald-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-emerald-100 transition-colors focus:outline-none focus:ring-2 focus:ring-emerald-400 focus:ring-offset-2 focus:ring-offset-emerald-50">
                 <span className="relative flex h-3 w-3">
-                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-300 opacity-75"></span>
-                  <span className="relative inline-flex rounded-full h-3 w-3 bg-emerald-400"></span>
+                  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-emerald-300 opacity-75"></span>
+                  <span className="relative inline-flex h-3 w-3 rounded-full bg-emerald-400"></span>
                 </span>
                 Live Preview
               </button>
             </Link>
           ) : (
             <div>
-              <button className="inline-flex px-6 h-12 items-center justify-center gap-2 rounded-lg border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] text-slate-800 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 w-full pointer-events-none">
+              <button className="pointer-events-none inline-flex h-12 w-full items-center justify-center gap-2 rounded-lg border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-slate-800 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                 <span className="flex h-3 w-3">
-                  <span className="relative inline-flex rounded-full h-3 w-3 bg-slate-800"></span>
+                  <span className="relative inline-flex h-3 w-3 rounded-full bg-slate-800"></span>
                 </span>
                 Live Preview
               </button>
